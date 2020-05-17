c1d7d453df93576011f240e98ac815f91057cb28
Hell13Cat <blacktigermaxx@gmail.com>
2020-05-17 16:36:53 +0300

    ver 1.0

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# cprint
-Add-on for Python 3 colorama module
+
+
+
+
+
+
+
+
+
+
+
+
+
+# CPrint
+
+- [Русский язык](#rus)
+- [English language](#eng)
+
+## <a name="eng"></a> English language
+
+Multicolored text output
+
+### Import module
+
+```
+from cprint import cprint
+```
+
+### Syntax
+
+This module is used in the same way as regular print, but with some differences.
+
+- The call is made using
+
+```
+cprint(text)
+```
+
+- Starting from the place where you want to change the color, you need to insert text according to the scheme
+
+```
+~{text color:background color:modifier}~
+```
+
+Parameters can be released if you do not need to change this part 
+
+- To reset formatting, add
+
+```
+~{reset}~
+```
+
+### Parameter Values
+
+- Background and text
+
+```
+black
+red
+green
+yellow
+blue
+magneta
+cyan
+white
+n
+```
+
+- Modifiers
+
+```
+l - Thin lines
+n - Standard view
+b - Thumbnail
+```
+
+## <a name="rus"></a> Русский язык
+
+Вывод разноцветного текста
+
+### Импорт модуля
+
+```
+from cprint import cprint
+```
+
+### Синтаксис
+
+Этот модуль испозуется так же как и обычный print, но с некоторыми отличиями
+
+- Вызов происходит с помощью
+
+```
+cprint(text)
+```
+
+- Начиная с того места где нужно поменять цвет необходимо вставить текст по схеме
+
+```
+~{цвет текста:цвет фона:модификатор}~
+```
+
+Параметры можно отпускать, если не нужно менять эту часть 
+
+- Для сброса форматирования добавьте
+
+```
+~{reset}~
+```
+
+### Значения параметров
+
+- Фон и текст
+
+```
+black
+red
+green
+yellow
+blue
+magneta
+cyan
+white
+n
+```
+
+- Модификаторы
+
+```
+l - Тонкие линии
+n - Стандартный вид
+b - Жирный текст
+```
---
+++ cprint.py
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+try:
+    from colorama import init, Fore, Back, Style
+except:
+    import os
+    print("!-> Install the required module <-!")
+    os.system("pip install colorama")
+    print("!-> Done <-!")
+from colorama import init, Fore, Back, Style 
+
+def font(par):
+    tmp02 = {"black":Fore.BLACK, "red":Fore.RED, "green":Fore.GREEN, "yellow":Fore.YELLOW, "blue":Fore.BLUE, "magneta":Fore.MAGENTA, "cyan":Fore.CYAN, "white":Fore.WHITE, "n":Fore.RESET}
+    return tmp02[par]
+
+def bag(par):
+    tmp02 = {"black":Back.BLACK, "red":Back.RED, "green":Back.GREEN, "yellow":Back.YELLOW, "blue":Back.BLUE, "magneta":Back.MAGENTA, "cyan":Back.CYAN, "white":Back.WHITE, "n":Back.RESET}
+    return tmp02[par]
+
+def mode(par):
+    tmp02 = {"l":Style.DIM, "n":Style.NORMAL, "b":Style.BRIGHT, "reset":Style.RESET_ALL}
+    return tmp02[par]
+
+def create_one(text):
+    tmp01 = text.split("}~")
+    params = tmp01[0]
+    del tmp01[0]
+    only_text = "}~".join(tmp01)
+    if params == "reset":
+        return mode("reset") + only_text
+    text_ready = ""
+    pars_params = params.split(":")
+    if pars_params[0] != "":
+        text_ready = font(pars_params[0]) + text_ready
+    if pars_params[1] != "":
+        text_ready = bag(pars_params[1]) + text_ready
+    if pars_params[2] != "":
+        text_ready = mode(pars_params[2]) + text_ready
+    return text_ready + only_text
+
+def cprint(text):
+    if "~{" not in text:
+        print(text)
+        return 0
+    pars_text = text.split("~{")
+    ready_text = ""
+    for one_pars in pars_text:
+        if one_pars == "":
+            continue
+        ready_text += create_one(one_pars)
+    init(autoreset=True)
+    print (ready_text + mode("reset"))
+
+__all__ = ['cprint']
